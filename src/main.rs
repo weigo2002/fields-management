838a532e77ff4379db8060de5fa4e088e4d357dd
weigo0510 <weigo0510@outlook.com>
2024-03-19 21:02:08 +0800

    Init code

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use axum::{response::IntoResponse, routing::get, Json, Router};
+
+async fn health_check_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "Server is running";
+
+    let json_response = serde_json::json!({
+        "status": "OK",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/health", get(health_check_handler));
+    println!("Server starting...");
+    let listner = tokio::net::TcpListener::bind("0.0.0.0:42001")
+        .await
+        .unwrap();
+    axum::serve(listner, app).await.unwrap();
+}
